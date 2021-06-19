016538c6db88b00aaec917fc940ff2cf810149bb
mariya <mariyakatk@gmail.com>
2021-06-19 20:16:50 +0300

    add mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+// Установим подключение по умолчанию
+var mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB);
+// Позволим Mongoose использовать глобальную библиотеку промисов
+mongoose.Promise = global.Promise;
+// Получение подключения по умолчанию
+var db = mongoose.connection;
+
+// Привязать подключение к событию ошибки  (получать сообщения об ошибках подключения)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
@@ -8,5 +20,7 @@ app.get('/', function(req, res) {
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 });
+
+
 
 
